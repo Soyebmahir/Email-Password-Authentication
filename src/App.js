64c51c250782962a33568ec4b23f4f04c95d6188
Soyeb <tssoyeb@gmail.com>
2022-04-09 19:32:44 +0600

    value collect from input field

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import app from './firebase.init';
+import { getAuth } from "firebase/auth";
+
+
+const auth = getAuth (app)
 function App() {
+
+  const handleEmailBlur=(event)=>{
+  console.log(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="">
+        <input onBlur={handleEmailBlur} type="email" name="" id="" />
+        <br></br>
+        <input type="password" name="" id="" />
+      </form>
+      
     </div>
   );
 }

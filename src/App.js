85424aa5c2ee5e5ed091eebc6854ccab5638835e
Soyeb <tssoyeb@gmail.com>
2022-04-11 10:02:56 +0600

    form input toggle/Email varification/password reset

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import app from './firebase.init';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Form } from 'react-bootstrap';
 import { useState } from 'react';
@@ -12,9 +12,13 @@ function App() {
   const [validated, setValidated] = useState(false);
   const [registered,setRegistered]=useState(false)
   const [error,setError]=useState('');
-
+  const [name,setName]=useState('')
   const [email,setEmail]=useState('')
   const [password,setPassword]=useState('')
+
+  const handleNameBlur =(event)=>{
+    setName(event.target.value)
+  }
 
   const handleEmailBlur=(event)=>{
   setEmail(event.target.value);
@@ -23,7 +27,7 @@ function App() {
     setPassword(event.target.value);
   }
   const handleCheckRegistered =event=>{
-    console.log(event.target.checked)
+    setRegistered(event.target.checked)
   }
   const handleSubmitForm =(event)=>{
     event.preventDefault();
@@ -37,28 +41,81 @@ function App() {
       setError('Insert at least one digit')
       return
     }
+    setValidated(true);
     setError('')
 
-    setValidated(true);
-
-    createUserWithEmailAndPassword(auth,email,password)
-    .then(result=>{
-      const user =result.user;
-      console.log(user);
-    })
-    .catch(error =>{
-      console.error(error)
-      setError(error.message)
-    })
+    if(registered){
+      console.log("login");
+      signInWithEmailAndPassword(auth,email,password)
+      .then(result=>{
+        const user =result.user;
+        console.log(user);
+      })
+      .catch(error=>{
+        console.log(error);
+        setError(error.message)
+      })
+    }else{
+      createUserWithEmailAndPassword(auth,email,password)
+      .then(result=>{
+        const user =result.user;
+        console.log(user);
+        setEmail('')
+        setPassword('')
+        setName('')
+        updateUserName();
+        varification();
+      })
+      .catch(error =>{
+        console.error(error)
+        setError(error.message)
+      })
+    }
+   
     event.preventDefault();
 
+  }
+
+  const updateUserName =()=>{
+    updateProfile(auth.currentUser,{
+      displayName: name
+    })
+    .then(()=>{
+      console.log("updated");
+    })
+    .catch(error=>{
+      console.log(error);
+    })
+  }
+
+  const handleForgetPassword =()=>{
+    sendPasswordResetEmail(auth,email)
+    .then(()=>{
+      console.log("password link sent");
+    })
+  }
+
+  const varification =()=>{
+    sendEmailVerification(auth.currentUser)
+    .then(result =>{
+      console.log("Email verification Sent");
+    })
+    .catch()
   }
 
   return (
     <div className="App">
       <div className='form-container mx-auto'>
-        <h1 className='text-success mt-5'>Please Login First</h1>
+        <h1 className='text-success mt-5'>Please {registered? "Login ":"Register First"}</h1>
       <Form noValidate validated={validated} onSubmit={handleSubmitForm}>
+  { !registered && <Form.Group className="mb-3" controlId="formBasicName">
+    <Form.Label>Your Name</Form.Label>
+    <Form.Control onBlur={handleNameBlur} type="name" placeholder="Enter Name" required/>
+   <Form.Control.Feedback type="invalid">
+            Please provide a name.
+          </Form.Control.Feedback>
+  </Form.Group>
+  }
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
     <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required/>
@@ -78,8 +135,10 @@ function App() {
     <Form.Check onChange={handleCheckRegistered} type="checkbox" label="Already Registered ?" />
   </Form.Group>
   </Form.Group>
+  <Button onClick={handleForgetPassword} variant="link">Forget Password ?</Button>
+  <br></br>
   <Button variant="primary" type="submit">
-    Register
+    {registered?'Login': "Register"}
   </Button>
 </Form>
       </div>
